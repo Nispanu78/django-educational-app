2d2b409c63deb3eeb540050fdcf78310d18226d0
Nicola <nicspanu@gmail.com>
2021-09-16 15:07:37 +0200

    admin

--- my_env/educa/courses/admin.py
+++ my_env/educa/courses/admin.py
@@ -6,3 +6,7 @@ from .models import Subject, Course, Module
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
     prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
