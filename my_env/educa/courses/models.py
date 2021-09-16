d6a2295765b773507b2013068b27e65237bcf9ac
Nicola <nicspanu@gmail.com>
2021-09-16 16:08:37 +0200

    contenttypes

--- my_env/educa/courses/models.py
+++ my_env/educa/courses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.contrib.contenttypes.models import ContentType
-# from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # from .fields import OrderField
 
 
@@ -48,3 +48,14 @@ class Module(models.Model):
 
     # class Meta:
     #     ordering = ['order']
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    # order = OrderField(blank=True, for_fields=['module'])
