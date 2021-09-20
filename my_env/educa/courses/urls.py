498e2e6f20750cffb120300556dcc6bab96d2ef7
Nicola <nicspanu@gmail.com>
2021-09-20 15:37:24 +0200

    displaying courses

--- my_env/educa/courses/urls.py
+++ my_env/educa/courses/urls.py
@@ -46,4 +46,12 @@ urlpatterns = [
           views.ContentOrderView.as_view(),
           name='content_order'),
 
+     path('subject/<slug:subject>/',
+         views.CourseListView.as_view(),
+         name='course_list_subject'),
+
+    path('<slug:slug>/',
+         views.CourseDetailView.as_view(),
+         name='course_detail'),
+
 ]
