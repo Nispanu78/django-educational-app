60cf6d359b3cc139988406bfa73340d565d924a2
Nicola <nicspanu@gmail.com>
2021-09-21 11:44:54 +0200

    views and templates

--- my_env/educa/courses/views.py
+++ my_env/educa/courses/views.py
@@ -12,6 +12,7 @@ from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
+from students.forms import CourseEnrollForm
 from django.db.models import Count
 
 
@@ -201,3 +202,9 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+                                   initial={'course':self.object})
+        return context
