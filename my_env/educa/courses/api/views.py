61892374ddc42182ba749da6021cb09c5fdac08b
Nicola <nicspanu@gmail.com>
2021-09-24 11:31:03 +0200

    views

--- my_env/educa/courses/api/views.py
+++ my_env/educa/courses/api/views.py
@@ -1,7 +1,7 @@
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # from rest_framework.authentication import BasicAuthentication
 # from rest_framework.permissions import IsAuthenticated
 # from rest_framework import viewsets
@@ -21,14 +21,14 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-# class CourseEnrollView(APIView):
-#     authentication_classes = (BasicAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def post(self, request, pk, format=None):
-#         course = get_object_or_404(Course, pk=pk)
-#         course.students.add(request.user)
-#         return Response({'enrolled': True})
+class CourseEnrollView(APIView):
+    # authentication_classes = (BasicAuthentication,)
+    # permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
 #
 #
 # class CourseViewSet(viewsets.ReadOnlyModelViewSet):
