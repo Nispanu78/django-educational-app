c014eb48cd2d2d1c03aa5a7cb81fe2f9d010c99b
Nicola <nicspanu@gmail.com>
2021-09-24 11:38:53 +0200

    authentication

--- my_env/educa/courses/api/views.py
+++ my_env/educa/courses/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication
 # from rest_framework.permissions import IsAuthenticated
 # from rest_framework import viewsets
 # from rest_framework.decorators import action
@@ -22,7 +22,7 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
-    # authentication_classes = (BasicAuthentication,)
+    authentication_classes = (BasicAuthentication,)
     # permission_classes = (IsAuthenticated,)
 
     def post(self, request, pk, format=None):
